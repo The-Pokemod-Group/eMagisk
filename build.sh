525ae4f1a5ddf9ac1611584a405b25b12be232c2
emi~ <esauvisky@gmail.com>
2022-06-25 22:40:25 -0300

    fix ci again

--- build.sh
+++ build.sh
@@ -6,10 +6,14 @@ cd "$DIRNAME"
 
 ver=$(sed -n "s|^versionCode=||p" module.prop)
 name=$(sed -n "s|^name=||p" module.prop | sed "s| |-|g")
-newVerCode=$((ver + 1))
-newVersion=$(echo $newVerCode | \sed 's|\(.\)\(.\)\(.\)|\1\.\2\.\3|')
-sed --in-place "s|^versionCode=$ver|versionCode=$newVerCode|;s|^version=v.*|version=v$newVersion|" module.prop
+if [[ $1 == 'github' ]]; then
+    newVerCode="$ver"
+else
+    newVerCode=$((ver + 1))
+    newVersion=$(echo $newVerCode | \sed 's|\(.\)\(.\)\(.\)|\1\.\2\.\3|')
+fi
 
+sed --in-place "s|^versionCode=$ver|versionCode=$newVerCode|;s|^version=v.*|version=v$newVersion|" module.prop
 zip -r "$name-$newVersion".zip . -x ".git/*" "LICENSE" "build.sh" ".gitignore" "*.zip"
 # echo "$newVerCode" >../Deploy/version
 echo "Made $name-$newVersion ($newVerCode)"
